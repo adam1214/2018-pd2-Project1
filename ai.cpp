8b50ec4dd509290267eaa3f70395c2b515de1755
F64051114 <f64051114@mail.ncku.edu.tw>
2018-03-23 00:22:40 +0800

    coding style

--- ai.cpp
+++ ai.cpp
@@ -3,45 +3,36 @@
 #include<cstdio>
 #include<cstdlib>
 using namespace std;
-int fun(char p1,char p2,char p3,char p4,int mana)
-{
-	if((p1=='7'||p2=='7'||p3=='7'||p4=='7')&&mana>=8)
-	{
+int fun(char p1,char p2,char p3,char p4,int mana) {
+	if((p1=='7'||p2=='7'||p3=='7'||p4=='7')&&mana>=8) {
 		printf("1 7 20 4\n");
 		return 1;
 	}
-	if((p1=='4'||p2=='4'||p3=='4'||p4=='4')&&mana>=6)
-	{
+	if((p1=='4'||p2=='4'||p3=='4'||p4=='4')&&mana>=6) {
 		 printf("1 4 20 4\n");
 		 return 1;
 	}
-	if((p1=='C'||p2=='C'||p3=='C'||p4=='C')&&mana>=7)
-	{
+	if((p1=='C'||p2=='C'||p3=='C'||p4=='C')&&mana>=7) {
 		printf("1 C 2 5\n");
 		return 1;
 	}
-	if((p1=='2'||p2=='2'||p3=='2'||p4=='2')&&mana>=3)
-	{
+	if((p1=='2'||p2=='2'||p3=='2'||p4=='2')&&mana>=3) { 
 		printf("1 2 2 5\n");
 		return 1;
 	}
-	if((p1=='1'||p2=='1'||p3=='1'||p4=='1')&&mana>=5)
-	{
+	if((p1=='1'||p2=='1'||p3=='1'||p4=='1')&&mana>=5) {
 		printf("1 1 20 4\n");
 		return 1;
 	}
-	if((p1=='6'||p2=='6'||p3=='6'||p4=='6')&&mana>=4)
-	{
+	if((p1=='6'||p2=='6'||p3=='6'||p4=='6')&&mana>=4) {
 		printf("1 6 2 5");
 		return 1;
 	}
-	if((p1=='5'||p2=='5'||p3=='5'||p4=='5')&&mana>=1)
-	{
+	if((p1=='5'||p2=='5'||p3=='5'||p4=='5')&&mana>=1) {
 		printf("1 5 2 5\n");
 		return 1;
 	}
-	else
-	{
+	else {
 		return 0;
 	}
 }
