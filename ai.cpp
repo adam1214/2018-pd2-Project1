f2d7f79444880ef36172e701575412afff102899
F64051114 <f64051114@mail.ncku.edu.tw>
2018-03-23 00:55:51 +0800

    攻擊兵放前面，防守兵放上面

--- ai.cpp
+++ ai.cpp
@@ -5,31 +5,31 @@
 using namespace std;
 int fun(char p1,char p2,char p3,char p4,int mana) {
     if((p1=='7'||p2=='7'||p3=='7'||p4=='7')&&mana>=8) {
-        printf("1 7 20 4\n");
+        printf("1 7 7 7\n");
         return 1;
     }
     if((p1=='4'||p2=='4'||p3=='4'||p4=='4')&&mana>=6) {
-        printf("1 4 20 4\n");
+        printf("1 4 7 7\n");
         return 1;
     }
     if((p1=='C'||p2=='C'||p3=='C'||p4=='C')&&mana>=7) {
-        printf("1 C 10 40\n");
+        printf("1 C 14 24\n");
         return 1;
     }
     if((p1=='2'||p2=='2'||p3=='2'||p4=='2')&&mana>=3) {
-        printf("1 2 10 40\n");
+        printf("1 2 14 24\n");
         return 1;
     }
     if((p1=='1'||p2=='1'||p3=='1'||p4=='1')&&mana>=5) {
-        printf("1 1 20 4\n");
+        printf("1 1 7 7\n");
         return 1;
     }
     if((p1=='6'||p2=='6'||p3=='6'||p4=='6')&&mana>=4) {
-        printf("1 6 10 40");
+        printf("1 6 14 24");
         return 1;
     }
     if((p1=='5'||p2=='5'||p3=='5'||p4=='5')&&mana>=1) {
-        printf("1 5 10 40\n");
+        printf("1 5 14 24\n");
         return 1;
     } else {
         return 0;
@@ -40,7 +40,7 @@ int main() {
     string str;
     int cnt=1;
     char p1,p2,p3,p4;
-    cout<<"1 2 9 C 4 5 6 3\n";
+    cout<<"1 2 9 C 4 5 6 7\n";
     getline(cin,str);
     getline(cin,str);
     scanf("MANA %d\n",&mana);
